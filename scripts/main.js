167cebf3d82f3d8ecc482edc3a37668f76c0d5c3
tassaddar <tassaddar-90@yandex.ru>
2017-02-28 18:37:30 +0300

    added alter image JS code

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  var mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/firefox-icon.jpg') {
+    myImage.setAttribute('src','images/firefox2.jpg');
+  } else {
+    myImage.setAttribute('src','images/firefox-icon.jpg');
+  }
+}
