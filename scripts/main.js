bf3703a1b60e1053cd4b014a88701095e5b2709a
tassaddar <tassaddar-90@yandex.ru>
2017-02-28 19:29:24 +0300

    Added name prompt

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+
 var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
